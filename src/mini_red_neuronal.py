095104af5a2e6d37dfc8df54e047ffc4cd12d224
Miguel S <m.angel.s.o@hotmail.com>
2025-08-05 22:33:42 -0500

    Creating an small network

--- src/mini_red_neuronal.py
+++ src/mini_red_neuronal.py
@@ -1,5 +1,12 @@
 import numpy as np
-imput = [[0,0]]
-layers = [3,4,5,1]
+data = np.array([[0,0,0], [0,1,1], [1,0,1], [1,1,0]])
+layers = [2,4,5,1]
 weights = [np.random.rand(x, y) for x, y in zip(layers[:-1], layers[1:])]
 biases = [np.random.rand(1, y) for y in layers[1:]]
+
+X = data[:, :2]
+y = data[:, 2:]
+activations = []
+a = X
+for i in range(1, len(layers)):
+    z = weights[i-1]
