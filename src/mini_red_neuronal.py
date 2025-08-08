5826f8c14d6d01a9d8b0c2b8dacb2fbc3204b7fd
Miguel S <m.angel.s.o@hotmail.com>
2025-08-07 23:08:31 -0500

    Training the neural network with a XOR, or & and test

--- src/mini_red_neuronal.py
+++ src/mini_red_neuronal.py
@@ -1,7 +1,8 @@
 import numpy as np
 import math
 
-data = np.array([[0,0,0], [0,1,1], [1,0,1], [1,1,0]])
+# data = np.array([[0,0,0], [0,1,1], [1,0,1], [1,1,0]])
+data = np.array([[0,0,0]])
 layers = [2,4,5,1]
 weights = [np.random.rand(x, y) for x, y in zip(layers[:-1], layers[1:])]
 biases = [np.random.rand(1, y) for y in layers[1:]]
@@ -9,10 +10,11 @@ biases = [np.random.rand(1, y) for y in layers[1:]]
 def sigmoid(z):
     return 1 / (1+np.exp(z))
 
+def dev_sig(a):
+    return a*(1-a)
+
 def output_error(a, y):
-    dev_sig = a*(1-a)
-    dev_cost = a - y
-    return dev_cost * dev_sig
+    return (a - y) * dev_sig(a)
 
 X = data[:, :2]
 y = data[:, 2:]
@@ -29,10 +31,27 @@ for i in range(len(activations)):
     print(activations[i])
     print('\n')
 
-n_layers = len(layers)-1
-errors = np.zeros((1, n_layers))
-errors[0, n_layers] = output_error(a, y)
+n_layers = len(layers)
+errors = [output_error(a, y)]
 print('Output error: \n', errors)
 
-for idx in range(len(activations)-1, 0):
-    error = np.dot(np.transpose(weights[idx]), )+biases[i]
+for idx in range(2, n_layers):
+    error = np.dot(errors[-idx+1], weights[-idx+1].T)*dev_sig(activations[-idx])
+    errors.insert(0, error)
+
+print('\nlayer errors')
+for error in errors:
+    print(error)
+n_data = len(X)
+
+print('\nweights and bias before: \n')
+for w in weights: print(w)
+for b in biases: print(b)
+
+for idx in range(0,n_layers-1):
+    weights[idx] = weights[idx] - (lr/n_data)*np.sum(np.dot(errors[idx]*activations[idx].T))
+    biases[idx] = biases[idx] - (lr/n_data)*np.sum(errors[idx])
+    
+print('\nweights and bias after: \n')
+for w in weights: print(w)
+for b in biases: print(b)
--- src/network.py
+++ src/network.py
@@ -1,25 +1,70 @@
 import numpy as np
-import matplotlib as mpl
 import math
 import random
-
+import os
 class Network():
     def __init__(self, layers):
         self.layers = layers
         self.weights = [np.random.rand(x, y) for x, y in zip(layers[:-1], layers[1:])]
         self.biases = [np.random.rand(1, y) for y in layers[1:]]
 
-    def sigmoid(z):
-        return 1 / (1+math.exp(z))
+    def sigmoid(self, z):
+        return 1 / (1+np.exp(z))
     
-    def GD(training_data, epochs, batch_zise,lr):
+    def dev_sig(self, a):
+        return a*(1-a)
+
+    def output_error(self, a, y):
+        return (a - y) * self.dev_sig(a)
+    
+    def SGD(self, training_data, epochs, batch_size, lr):
         n = len(training_data)
-        for i in epochs:
-            batch_data = [
-                training_data[d:d+batch_zise]
-                for d in range(0, n, batch_zise)
-            ]
-            random.shuffle(training_data)
-            for data in batch_data:
-                a = data[0]
-                z = 
+        for _ in range(epochs):
+            random.shuffle( training_data )
+            batches = [ training_data [i:i+ batch_size ] for i in range(0, n,
+            batch_size )]
+            for batch in batches:
+                for x, y in batch:
+                    activations = self.feedforward(x)
+                    errors = self.backpropagate_error(activations, y)
+                    n_data = len(x)
+                    self.GD(errors, activations, lr, n_data)
+            
+    
+    def feedforward(self, X):
+        activations = [X]
+        for i in range(0, len(self.layers)-1):
+            z = np.dot(activations[i], self.weights[i])+self.biases[i]
+            a = self.sigmoid(z)
+            activations.append(a)
+        return activations
+    
+    def backpropagate_error(self, activations, y):
+        output = activations[len(activations)-1]
+        errors = [(output - y) * self.dev_sig(output)]
+        n_layers = len(self.layers)
+        for idx in range(2, n_layers):
+            error = np.dot(errors[-idx+1], self.weights[-idx+1].T)*self.dev_sig(activations[-idx])
+            errors.insert(0, error)
+        return errors
+
+    def GD(self, errors, activations, lr, n_data):
+        for idx in range(0, len(self.layers)-1):
+            self.weights[idx] = self.weights[idx] - (lr/n_data)*np.sum(np.dot(errors[idx], activations[idx].T))
+            self.biases[idx] = self.biases[idx] - (lr/n_data)*np.sum(errors[idx])
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ruta_archivo = os.path.join(BASE_DIR, "../data/xor_test.csv")
+data = np.loadtxt(ruta_archivo, delimiter=',', dtype=int)
+X = data[:, :-1]
+y = data[:, -1] 
+
+dataset = [(X[i], [y[i]]) for i in range(len(X))]
+
+train_size = int(len(dataset) * 0.8)
+train_data = dataset[:train_size]
+test_data = dataset[train_size:]
+
+network1 = Network([2,5,5,1])
+network1.SGD(train_data, 10, 25, 0.25)
+
