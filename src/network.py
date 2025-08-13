7658ff71ce84b44aa22b6c8a990411158a065da3
Miguel S <m.angel.s.o@hotmail.com>
2025-08-13 00:28:11 -0500

    Adding the cost function metric to the network

--- src/network.py
+++ src/network.py
@@ -9,7 +9,10 @@ class Network():
         self.layers = layers
         self.weights = [np.random.randn(layers[i], layers[i+1]) for i in range(len(layers)-1)]
         self.biases = [np.zeros((1, layers[i+1])) for i in range(len(layers)-1)]
-        self.cost = cost
+        if cost == 'quadratic':
+            self.cost = self.quadratuc_cost
+        elif cost == 'cross entropy':
+            self.cost = self.cross_entropy_cost
 
     def sigmoid(self, z):
         return 1 / (1+np.exp(-z))
@@ -17,7 +20,7 @@ class Network():
     def dev_sig(self, a):
         return a*(1-a)
     
-    def quadratuc_cost(self, y_true, y_pred, derivate=False):
+    def quadratic_cost(self, y_true, y_pred, derivate=False):
         if derivate:
             delta = (y_pred - y_true) * self.dev_sig(y_pred)
             return delta
@@ -30,21 +33,27 @@ class Network():
         if derivate:
             return y_pred-y_true
         else:
-            delta = np.sum(np.nan_to_num(-y_true*np.log(y_pred)-(1-y_true)*np.log(1-y_pred)))
-            return delta
+            cost = np.sum(np.nan_to_num(-y_true*np.log(y_pred)-(1-y_true)*np.log(1-y_pred)))
+            return cost
 
-    def SGD(self, training_data, epochs=10, batch_size=32, lr=0.1):
+    def SGD(self, training_data, epochs=10, batch_size=32, lr=0.1, return_training_cost=False):
         n = len(training_data)
+        all_training_costs = []
         for _ in range(epochs):
             random.shuffle( training_data )
             batches = [ training_data [i:i+ batch_size ] for i in range(0, n, batch_size)]
+            epoch_cost = []
             for batch in batches:
                 X_batch = np.vstack([x for x, _ in batch])
                 y_batch = np.vstack([y for _, y in batch])
                 activations = self.feedforward(X_batch)
                 grads_w, grads_b = self.backpropagation(activations, y_batch, self.cost)
                 self.update_w_b(grads_w, grads_b, lr, X_batch.shape[0])
-            
+                if return_training_cost:
+                    cost = self.cost_function(activations, y_batch)
+                    epoch_cost.append(cost)
+            if return_training_cost:
+                all_training_costs.append(np.mean(epoch_cost) if epoch_cost else 0)
     
     def feedforward(self, X):
         activations = [X]
@@ -55,16 +64,13 @@ class Network():
             activations.append(a)
         return activations
     
-    def backpropagation(self, activations, y, cost):
+    def backpropagation(self, activations, y):
         m = y.shape[0]
         n = len(self.weights)
         grads_w = [np.zeros_like(W) for W in self.weights]
         grads_b = [np.zeros_like(b) for b in self.biases]
         
-        if cost == 'quadratic':
-            delta = self.quadratuc_cost(y, activations[-1], derivate=True)
-        elif cost == 'cross entropy':
-            delta = self.cross_entropy_cost(y, activations[-1], derivate=True)
+        delta = self.cost(y, activations[-1], derivate=True)
         
         grads_w[-1] = np.dot(activations[-2].T, delta)
         grads_b[-1] = np.sum(delta, axis=0, keepdims=True)
@@ -79,6 +85,11 @@ class Network():
         for i in range(len(self.weights)):
             self.weights[i] -= (lr/batch_size)*grads_w[i]
             self.biases[i] -= (lr/batch_size)*grads_b[i]
+            
+    def cost_function(activations, y):
+        y_pred = activations[-1]
+        cost = self.cost(y, y_pred, derivate=False)/len(y_pred)
+        return cost  
         
     def predict(self, X, y=None):
         if isinstance(X, list) and isinstance(X[0], tuple):
